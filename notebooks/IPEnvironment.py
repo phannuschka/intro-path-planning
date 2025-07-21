29ca38e678c843ccea10d3dba4c5924ca39531e1
phannuschka <paul.hannuschka@gmail.com>
2025-07-21 18:53:00 +0200

    add draw collion objects for openCV visu

--- notebooks/IPEnvironment.py
+++ notebooks/IPEnvironment.py
@@ -5,6 +5,7 @@ This code is part of a series of notebooks regarding  "Introduction to robot pat
 
 License is based on Creative Commons: Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) (pls. check: http://creativecommons.org/licenses/by-nc/4.0/)
 """
+import cv2
 
 from IPPerfMonitor import IPPerfMonitor
 
@@ -67,4 +68,58 @@ class CollisionChecker(object):
     def drawObstacles(self, ax):
         for key, value in self.scene.items():
             plotting.plot_polygon(value, add_points=False, color='red')
-            
+
+
+    def drawOnImage(self, img, scale, offset, obstacle_color=(100, 100, 255)):
+        """Draw obstacles on OpenCV image
+
+        Args:
+            img: OpenCV image to draw on
+            scale: scaling factor from world coordinates to image coordinates
+            offset: (x_offset, y_offset) tuple for image positioning
+            obstacle_color: BGR color tuple for obstacles (default red)
+
+        Returns:
+            img: The modified image
+        """
+        x_offset, y_offset = offset
+
+        for key, value in self.scene.items():
+            if hasattr(value, 'exterior'):
+                # It's a Polygon
+                coords = list(value.exterior.coords)
+
+                # Convert world coordinates to image coordinates
+                img_coords = []
+                for x, y in coords:
+                    img_x = int(x * scale + x_offset)
+                    img_y = int(img.shape[0] - (y * scale + y_offset))
+                    img_coords.append([img_x, img_y])
+
+                # Draw filled polygon
+                pts = np.array(img_coords, np.int32)
+                cv2.fillPoly(img, [pts], obstacle_color)
+
+            elif hasattr(value, 'coords'):
+                # It might be a LineString or Point
+                coords = list(value.coords)
+                if len(coords) > 1:
+                    # LineString - draw as connected lines
+                    img_coords = []
+                    for x, y in coords:
+                        img_x = int(x * scale + x_offset)
+                        img_y = int(img.shape[0] - (y * scale + y_offset))
+                        img_coords.append((img_x, img_y))
+
+                    # Draw lines connecting the points
+                    for i in range(len(img_coords) - 1):
+                        cv2.line(img, img_coords[i], img_coords[i + 1], obstacle_color, 2)
+
+                elif len(coords) == 1:
+                    # Point - draw as small circle
+                    x, y = coords[0]
+                    img_x = int(x * scale + x_offset)
+                    img_y = int(img.shape[0] - (y * scale + y_offset))
+                    cv2.circle(img, (img_x, img_y), 3, obstacle_color, -1)
+
+        return img

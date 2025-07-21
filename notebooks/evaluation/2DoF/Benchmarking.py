d7952127f307c3f88c72458f70a87883333c82fb
SimonScharpf <urxuj@student.kit.edu>
2025-07-21 17:40:53 +0200

    clean up

--- notebooks/evaluation/2DoF/Benchmarking.py
+++ notebooks/evaluation/2DoF/Benchmarking.py
@@ -16,72 +16,78 @@ from IPPerfMonitor import IPPerfMonitor
 import time
 import json
 import networkx as nx
+from typing import List, Dict
 
-astarConfig = dict()
-astarConfig["w"] = .5
-astarConfig["heuristic"]  = "euclidean"
-astarConfig["reopen"] = True
-astarConfig["dof"] = 2
-astarConfig["discretization"] = [50 for _ in range(astarConfig["dof"])]
-astarConfig["check_connection"] = True
-
-configs = []
-configs.append(astarConfig)
-
-for benchmark in ts.benchList:
-    print(benchmark.name)
-    for config in configs:
-        benchmark: Benchmark
-        # Note: for 2 DOF and workspace = configuration space
-        config["lowLimits"] = [limit[0] for limit in benchmark.collisionChecker.getEnvironmentLimits()]
-        config["highLimits"] = [limit[1] for limit in benchmark.collisionChecker.getEnvironmentLimits()]
+def evaluate(configs: List[Dict]):
+    for benchmark in ts.benchList:
+        print(benchmark.name)
+        for config in configs:
+            benchmark: Benchmark
+            # Note: for 2 DOF and workspace = configuration space
+            config["lowLimits"] = [limit[0] for limit in benchmark.collisionChecker.getEnvironmentLimits()]
+            config["highLimits"] = [limit[1] for limit in benchmark.collisionChecker.getEnvironmentLimits()]
 
 
-        fig_local = plt.figure(figsize=(10,10))
-        ax = fig_local.add_subplot(1,1,1)
-        astar = AStar(benchmark.collisionChecker)
+            fig_local = plt.figure(figsize=(10,10))
+            ax = fig_local.add_subplot(1,1,1)
+            astar = AStar(benchmark.collisionChecker)
 
-        stats = {}
-        title = benchmark.name
-        
-        start = time.time()
-        solution, deltas = astar.planPath(benchmark.startList, benchmark.goalList, astarConfig, store_viz=True)
-        stats["execution_time"] = time.time() - start
-
-        stats["road_map_size"] = len(astar.graph.nodes.keys())
-
-        stats["num_nodes_solution_path"] = -1
-
-        stats["solution_path_length"] = -1
-        
-        if solution is not None:
-            stats["num_nodes_solution_path"] = len(solution)
+            stats = {}
+            title = benchmark.name
             
-            stats["solution_path_length"] = astar.graph.nodes[solution[-1]]["g"]
+            start = time.time()
+            solution, deltas = astar.planPath(benchmark.startList, benchmark.goalList, config, store_viz=True)
+            stats["execution_time"] = time.time() - start
 
-            title += " (No path found!)"
+            stats["road_map_size"] = len(astar.graph.nodes.keys())
 
-        # save stats
-        dir_name = f"{script_dir}/results/{benchmark.name}"
-        dir_name = f"{dir_name}/disc{astarConfig["discretization"]}_{astarConfig["heuristic"]}_w{astarConfig["w"]}"
-        if astarConfig["reopen"]:
-            dir_name += "_reopen"
-        if astarConfig["check_connection"]:
-            dir_name += "_linetest"
+            stats["num_nodes_solution_path"] = -1
 
-        if not os.path.exists(dir_name):
-            os.makedirs(dir_name)
+            stats["solution_path_length"] = -1
+            
+            if solution is not None:
+                stats["num_nodes_solution_path"] = len(solution)
+                
+                stats["solution_path_length"] = astar.graph.nodes[solution[-1]]["g"]
 
-        stats_filename = f"{dir_name}/stats.json"
-        with open(stats_filename, "w") as f:
-            json.dump(stats, f)
+                title += " (No path found!)"
 
-        deltas_filename = f"{dir_name}/deltas.json"
-        with open(deltas_filename, "w") as f:
-            json.dump(deltas, f)
-        
-        graph_filename = f"{dir_name}/graph.json"
-        with open(graph_filename, "w") as f:
-            json.dump(nx.node_link_data(astar.graph), f)
+            # save stats
+            dir_name = f"{script_dir}/results/{benchmark.name}"
+            dir_name = f"{dir_name}/disc{config["discretization"]}_{config["heuristic"]}_w{config["w"]}"
+            if config["reopen"]:
+                dir_name += "_reopen"
+            if config["check_connection"]:
+                dir_name += "_linetest"
+
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+
+            stats_filename = f"{dir_name}/stats.json"
+            with open(stats_filename, "w") as f:
+                json.dump(stats, f)
+
+            deltas_filename = f"{dir_name}/deltas.json"
+            with open(deltas_filename, "w") as f:
+                json.dump(deltas, f)
+            
+            graph_filename = f"{dir_name}/graph.json"
+            with open(graph_filename, "w") as f:
+                json.dump(nx.node_link_data(astar.graph), f)
+
+
+if __name__ == "__main__":
+    astarConfig = dict()
+    astarConfig["w"] = .5
+    astarConfig["heuristic"]  = "euclidean"
+    astarConfig["reopen"] = True
+    astarConfig["dof"] = 2
+    astarConfig["discretization"] = [50 for _ in range(astarConfig["dof"])]
+    astarConfig["check_connection"] = True
+
+    configs = []
+    configs.append(astarConfig)
+    
+    evaluate(configs=configs)
 
         

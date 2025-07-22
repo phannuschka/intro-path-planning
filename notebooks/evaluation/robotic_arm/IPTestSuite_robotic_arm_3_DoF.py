feff77e661a97157f2a5b839b3b1b764475e65ed
phannuschka <paul.hannuschka@gmail.com>
2025-07-22 14:06:13 +0200

    add first 3 DOF benchmark

--- notebooks/evaluation/robotic_arm/IPTestSuite_robotic_arm_3_DoF.py
+++ notebooks/evaluation/robotic_arm/IPTestSuite_robotic_arm_3_DoF.py
@@ -6,8 +6,38 @@ sys.path.append(os.path.abspath(notebooks_dir))
 
 from IPBenchmark import Benchmark 
 from IPEnvironment import CollisionChecker
+from IPEnvironmentKin import KinChainCollisionChecker
+from IPPlanarManipulator import PlanarRobot
 from shapely.geometry import Point, Polygon, LineString
 import random
 import numpy as np
 
+
+n_joints = 3
+robot = PlanarRobot(n_joints=n_joints, length=4.0)
+
 benchList = list()
+
+# -----------------------------------------
+
+obst = dict()
+obst["obs1"] = LineString([(-1, -4), (-1, 0.5)]).buffer(0.15)
+obst["obs2"] = LineString([(-1, 1.2), (-1, 4)]).buffer(0.15)
+obst["obs3"] = LineString([(-3.5, 1), (-2, 1)]).buffer(0.2)
+obst["obs4"] = LineString([(-3.5, 1), (-3.5, 3.5)]).buffer(0.2)
+
+environment = KinChainCollisionChecker(robot, obst, limits=[[-3.14,3.14]] * n_joints, fk_resolution=.2)
+start_joint_pos = [0.0] * 9
+start_joint_pos[0] = 0.0
+start_joint_pos = start_joint_pos[:n_joints]
+
+end_joint_pos = [0.0] * 9
+end_joint_pos[0] = 2.5
+end_joint_pos = end_joint_pos[:n_joints]
+
+description = "Planar manipulator with 3 DoF has to find a path around simple obstacles."
+
+benchmark = Benchmark("simple", environment, [start_joint_pos], [end_joint_pos], description, level=1)
+benchList.append(benchmark)
+
+# -----------------------------------------

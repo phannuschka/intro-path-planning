8f136ff4682484da4d8a83d400aa2541ebf7d998
phannuschka <paul.hannuschka@gmail.com>
2025-07-22 15:25:10 +0200

    add actually simple test case

--- notebooks/evaluation/robotic_arm/IPTestSuite_robotic_arm_3_DoF.py
+++ notebooks/evaluation/robotic_arm/IPTestSuite_robotic_arm_3_DoF.py
@@ -21,18 +21,17 @@ benchList = list()
 # -----------------------------------------
 
 obst = dict()
-obst["obs1"] = LineString([(-1, -4), (-1, 0.5)]).buffer(0.15)
-obst["obs2"] = LineString([(-1, 1.2), (-1, 4)]).buffer(0.15)
-obst["obs3"] = LineString([(-3.5, 1), (-2, 1)]).buffer(0.2)
-obst["obs4"] = LineString([(-3.5, 1), (-3.5, 3.5)]).buffer(0.2)
+obst["obs1"] = LineString([(-2, 0), (-0.8, 0)]).buffer(0.5)
+obst["obs2"] = LineString([(2, 0), (2, 1)]).buffer(0.2)
+obst["obs3"] = LineString([(-1, 2), (1, 2)]).buffer(0.1)
 
 environment = KinChainCollisionChecker(robot, obst, limits=[[-3.14,3.14]] * n_joints, fk_resolution=.2)
 start_joint_pos = [0.0] * 9
-start_joint_pos[0] = 0.0
+start_joint_pos[0] = 0.8
 start_joint_pos = start_joint_pos[:n_joints]
 
 end_joint_pos = [0.0] * 9
-end_joint_pos[0] = 2.5
+end_joint_pos[0] = 4
 end_joint_pos = end_joint_pos[:n_joints]
 
 description = "Planar manipulator has to find a path around simple obstacles."

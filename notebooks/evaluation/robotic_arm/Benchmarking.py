96ce5806a31974ab6cbe0153be61dea060f09ba7
SimonScharpf <urxuj@student.kit.edu>
2025-07-22 17:35:30 +0200

    modifiy benchmarking for more freedom in test cases to be executed

--- notebooks/evaluation/robotic_arm/Benchmarking.py
+++ notebooks/evaluation/robotic_arm/Benchmarking.py
@@ -21,78 +21,79 @@ import networkx as nx
 from typing import List, Dict
 import numpy as np
 
+testSuits = {3: ts3, 6: ts6, 9: ts9}
+
 def evaluate(configs: List[Dict]):
-    for i, ts in enumerate([ts3, ts6, ts9]):
+    for config in configs:
+        ts = testSuits[config["dof"]]
         for benchmark in ts.benchList:
             print(benchmark.name)
-            for config in configs:
-                benchmark: Benchmark
 
-                config["dof"] = (i + 1) * 3
-                config["lowLimits"] = [-2 *np.pi for _ in range(config["dof"])]
-                config["highLimits"] = [2 *np.pi for _ in range(config["dof"])]
-                config["discretization"] = [config["discretizationValue"] for _ in range(config["dof"])]
+            astar = AStar(benchmark.collisionChecker)
 
-                astar = AStar(benchmark.collisionChecker)
+            stats = {}
+            title = benchmark.name
+            
+            start = time.time()
+            solution, _ = astar.planPath(benchmark.startList, benchmark.goalList, config, store_viz=False)
+            stats["execution_time"] = time.time() - start
 
-                stats = {}
-                title = benchmark.name
+            stats["road_map_size"] = len(astar.graph.nodes.keys())
+
+            stats["num_nodes_solution_path"] = -1
+
+            stats["solution_path_length"] = -1
+            
+            if solution != []:
+                stats["num_nodes_solution_path"] = len(solution)
                 
-                start = time.time()
-                solution, _ = astar.planPath(benchmark.startList, benchmark.goalList, config, store_viz=False)
-                stats["execution_time"] = time.time() - start
+                stats["solution_path_length"] = astar.graph.nodes[solution[-1]]["g"]
 
-                stats["road_map_size"] = len(astar.graph.nodes.keys())
+                title += " (No path found!)"
 
-                stats["num_nodes_solution_path"] = -1
-
-                stats["solution_path_length"] = -1
+            # save stats
+            dir_name = f"{script_dir}/results/{config["dof"]}DoF/{benchmark.name}"
+            dir_name = f"{dir_name}/disc{config['discretization']}_{config['heuristic']}_w{config['w']}"
+            if config["reopen"]:
+                dir_name += "_reopen"
+            if config["check_connection"]:
+                dir_name += "_linetest"
+                if config["lazy_check_connection"]:
+                    dir_name += "_lazy"
                 
-                if solution != []:
-                    stats["num_nodes_solution_path"] = len(solution)
-                    
-                    stats["solution_path_length"] = astar.graph.nodes[solution[-1]]["g"]
 
-                    title += " (No path found!)"
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
 
-                # save stats
-                dir_name = f"{script_dir}/results/{3 * (i + 1)}DoF/{benchmark.name}"
-                dir_name = f"{dir_name}/disc{config['discretization']}_{config['heuristic']}_w{config['w']}"
-                if config["reopen"]:
-                    dir_name += "_reopen"
-                if config["check_connection"]:
-                    dir_name += "_linetest"
-                    if config["lazy_check_connection"]:
-                        dir_name += "_lazy"
-                    
+            stats_filename = f"{dir_name}/stats.json"
+            with open(stats_filename, "w") as f:
+                json.dump(stats, f)
+            
+            graph_filename = f"{dir_name}/graph.json"
+            with open(graph_filename, "w") as f:
+                json.dump(nx.node_link_data(astar.graph), f)
 
-                if not os.path.exists(dir_name):
-                    os.makedirs(dir_name)
-
-                stats_filename = f"{dir_name}/stats.json"
-                with open(stats_filename, "w") as f:
-                    json.dump(stats, f)
-                
-                graph_filename = f"{dir_name}/graph.json"
-                with open(graph_filename, "w") as f:
-                    json.dump(nx.node_link_data(astar.graph), f)
-
-                solution_filename = f"{dir_name}/solution.json"
-                with open(solution_filename, "w") as f:
-                    json.dump(solution, f)
+            solution_filename = f"{dir_name}/solution.json"
+            with open(solution_filename, "w") as f:
+                json.dump(solution, f)
 
 
 if __name__ == "__main__":
-    astarConfig = dict()
-    astarConfig["w"] = .5
-    astarConfig["heuristic"]  = "euclidean"
-    astarConfig["reopen"] = True
-    astarConfig["discretizationValue"] = 30
-    astarConfig["check_connection"] = True
-    astarConfig["lazy_check_connection"] = True
-
     configs = []
-    configs.append(astarConfig)
+    for i in range(1, 4):
+        astarConfig = dict()
+        astarConfig["dof"] = 3 * i
+        astarConfig["lowLimits"] = [-2 *np.pi for _ in range(astarConfig["dof"])]
+        astarConfig["highLimits"] = [2 *np.pi for _ in range(astarConfig["dof"])]
+        astarConfig["discretization"] = [20 for _ in range(astarConfig["dof"])]
+        astarConfig["w"] = .5
+        astarConfig["heuristic"]  = "euclidean"
+        astarConfig["reopen"] = True
+        astarConfig["discretizationValue"] = 20
+        astarConfig["check_connection"] = True
+        astarConfig["lazy_check_connection"] = True
+
+        configs.append(astarConfig)
     
     evaluate(configs=configs)
 

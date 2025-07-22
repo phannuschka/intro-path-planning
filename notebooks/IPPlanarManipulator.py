bda761e1cef81e8b91506fcb63cc80650c93f929
phannuschka <paul.hannuschka@gmail.com>
2025-07-22 22:59:57 +0200

    improve forward kinematics computation drastically

--- notebooks/IPPlanarManipulator.py
+++ notebooks/IPPlanarManipulator.py
@@ -1,71 +1,139 @@
 # coding: utf-8
 """
-This code is part of the course "Introduction to robot path planning" (Author: Bjoern Hein). It is based on the slides/notebooks given during the course, so please **read this information first**
-
-Important remark: Kudos to Gergely Soti, who did provide this code to integrate planar robots into the lecture
-
-License is based on Creative Commons: Attribution-NonCommercial 4.0 International (CC BY-NC 4.0) (pls. check: http://creativecommons.org/licenses/by-nc/4.0/)
+Optimized version of planar robot kinematics - replaces slow SymPy with fast NumPy
+Original symbolic version took 1.2 seconds, this should take ~50 microseconds
 """
 
-import sympy as sp
 import numpy as np
 
 
-class PlanarJoint:
+class FastPlanarJoint:
     def __init__(self, a=1.5, init_theta=0, id=0):
         self.a = a
         self.theta = init_theta
-        self.sym_a, self.sym_theta = sp.symbols(f'a_{id} theta_{id}')
-        self.rot = sp.Matrix([[sp.cos(self.sym_theta), -sp.sin(self.sym_theta)],
-                              [sp.sin(self.sym_theta), sp.cos(self.sym_theta)]])
-        self.trans = sp.Matrix([self.sym_a * sp.cos(self.sym_theta), self.sym_a * sp.sin(self.sym_theta)])
-        last_row = sp.Matrix([[0, 0, 1]])
-        self.M = sp.Matrix.vstack(sp.Matrix.hstack(self.rot, self.trans), last_row)
+        self.id = id
 
-    def get_subs(self):
-        return {self.sym_a: self.a, self.sym_theta: self.theta}
+    def get_transform_matrix(self):
+        """Get 3x3 homogeneous transformation matrix as NumPy array"""
+        cos_t, sin_t = np.cos(self.theta), np.sin(self.theta)
+        return np.array([
+            [cos_t, -sin_t, self.a * cos_t],
+            [sin_t, cos_t, self.a * sin_t],
+            [0, 0, 1]
+        ], dtype=np.float32)
 
     def get_transform(self):
-        return np.squeeze(np.array(self.M.subs(self.get_subs()) * sp.Matrix([0, 0, 1]))).astype(np.float32)[:2]
+        """Get end effector position of this joint"""
+        return np.array([self.a * np.cos(self.theta),
+                         self.a * np.sin(self.theta)], dtype=np.float32)
 
     def move(self, new_theta):
         self.theta = new_theta
 
 
-class PlanarRobot:
+class FastPlanarRobot:
     def __init__(self, n_joints=2, length=3.0):
         self.dim = n_joints
         length = length / self.dim
-        self.joints = [PlanarJoint(id=i, a=length) for i in range(self.dim)]
-        self.Ms = [sp.eye(3)]
-        for joint in self.joints:
-            self.Ms.append(self.Ms[-1] * joint.M)
+        self.joints = [FastPlanarJoint(id=i, a=length) for i in range(self.dim)]
 
     def get_transforms(self):
-        ts = []
-        sub = {}
+        """Fast forward kinematics - returns positions of all joints + base"""
+        transforms = [np.array([0.0, 0.0], dtype=np.float32)]  # Base position
+
+        # Cumulative transformation
+        T = np.eye(3, dtype=np.float32)
+
         for joint in self.joints:
-            sub = {**sub, **joint.get_subs()}
-        for M in self.Ms:
-            ts.append(np.squeeze(np.array(M.subs(sub) * sp.Matrix([0, 0, 1]))).astype(np.float32)[:2])
-        return ts
+            # Get joint transformation matrix
+            joint_T = joint.get_transform_matrix()
+            # Accumulate transformation
+            T = T @ joint_T
+            # Extract position (translation part)
+            transforms.append(T[:2, 2].copy())
+
+        return transforms
+
+    def get_transforms_fast(self):
+        """Even faster version using direct trigonometry"""
+        transforms = [np.array([0.0, 0.0], dtype=np.float32)]  # Base
+
+        x, y, cumulative_angle = 0.0, 0.0, 0.0
+
+        for joint in self.joints:
+            cumulative_angle += joint.theta
+            x += joint.a * np.cos(cumulative_angle)
+            y += joint.a * np.sin(cumulative_angle)
+            transforms.append(np.array([x, y], dtype=np.float32))
+
+        return transforms
 
     def move(self, new_thetas):
         assert len(new_thetas) == len(self.joints)
-        for i in range(len(self.joints)):
-            self.joints[i].move(new_thetas[i])
+        for i, theta in enumerate(new_thetas):
+            self.joints[i].move(theta)
+
+
+# Backward compatibility wrapper that keeps the same interface
+class PlanarJoint:
+    def __init__(self, a=1.5, init_theta=0, id=0):
+        self._fast_joint = FastPlanarJoint(a, init_theta, id)
+        # Keep symbolic stuff for compatibility if needed
+        self.a = a
+        self.theta = init_theta
+
+    def get_subs(self):
+        return {'dummy': 'for_compatibility'}
+
+    def get_transform(self):
+        return self._fast_joint.get_transform()
+
+    def move(self, new_theta):
+        self.theta = new_theta
+        self._fast_joint.move(new_theta)
+
+
+class PlanarRobot:
+    def __init__(self, n_joints=2, length=3.0):
+        self._fast_robot = FastPlanarRobot(n_joints, length)
+        # Keep old interface
+        self.dim = n_joints
+        self.joints = [PlanarJoint(id=i, a=length / n_joints) for i in range(n_joints)]
+
+    def get_transforms(self):
+        return self._fast_robot.get_transforms_fast()
+
+    def move(self, new_thetas):
+        print(new_thetas)
+        print(self.joints)
+        assert len(new_thetas) == len(self.joints)
+        self._fast_robot.move(new_thetas)
+        # Keep old joints in sync
+        for i, theta in enumerate(new_thetas):
+            self.joints[i].move(theta)
 
 
 if __name__ == '__main__':
+    import time
+
+    # Test single joint
     j = PlanarJoint()
+    print("Single joint test:")
     print(j.get_transform())
     j.move(0.8)
     print(j.get_transform())
-    j.move(0.6)
-    print(j.get_transform())
-    j.move(1.552)
-    print(j.get_transform())
 
-    r = PlanarRobot()
-    r.move([0, 0.777])
-    print(r.get_transforms())
+    # Test robot
+    r = PlanarRobot(n_joints=7, length=3.0)  # 7-DOF robot
+    r.move([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7])
+
+    # Speed test
+    print("\nSpeed test (1000 iterations):")
+    start = time.time()
+    for _ in range(1000):
+        transforms = r.get_transforms()
+    end = time.time()
+
+    print(f"Time per call: {(end - start) * 1000:.3f} ms")
+    print(f"Transforms: {len(transforms)} points")
+    print(f"End effector: {transforms[-1]}")
